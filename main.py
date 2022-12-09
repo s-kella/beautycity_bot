37ac8d1915be70ad10a4aa51869121c68abc32d4
s-kella <52741545+s-kella@users.noreply.github.com>
2022-12-08 19:01:14 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -27,8 +27,9 @@ def get_keyboard(buttons, one_time_keyboard=False):
 def start(update, context):
     user = update.message.from_user
     update.message.reply_text(
-        text=f'Привет! Добро пожаловать бот BeautyCity!')
+        text=f'Привет! Добро пожаловать в бот BeautyCity!')
     menu(update, context)
+    #registration(update, context)
 
 
 def menu(update, context):
@@ -100,6 +101,45 @@ def by_service(update, context):
     )
 
 
+def get_users_phone(update, context):
+    reply_markup = ReplyKeyboardMarkup([[KeyboardButton(str('Предоставить номер телефона'), request_contact=True)]],
+                                       resize_keyboard=True)
+    message = 'Предоставьте свой номер телефона'
+    context.bot.sendMessage(update.effective_chat.id, message, reply_markup=reply_markup)
+
+
+# TODO сделать, чтобы новые кнопки не появлялись, пока пользователь не даст телефон
+def registration(update, context):
+    buttons = ['Политика обработки данных', 'Я даю согласие на обработку данных']
+    reply_markup = get_keyboard(buttons)
+    users_personal_data = {
+        'first_name': update.message.from_user.first_name,
+        'last_name': update.message.from_user.last_name,
+        #'phone_number': get_users_phone(update, context)
+    }
+    update.message.reply_text(
+        text="Регистрация",
+        reply_markup=reply_markup,
+    )
+
+
+def send_file_policy(update, context):
+    context.bot.sendDocument(
+        chat_id=update.message.chat_id,
+        document=open('file.pdf', 'rb'),
+        caption='Политика обработки данных'
+    )
+
+
+def confirm_appointment(update, context):
+    buttons = ['Подтвердить запись', 'Назад']
+    reply_markup = get_keyboard(buttons)
+    update.message.reply_text(
+        text="Ваша запись:\n[дата]\n[услуга]\n[мастер]\n[салон]",
+        reply_markup=reply_markup,
+    )
+
+
 def message_handler(update, context):
     text = update.message.text
 
@@ -118,6 +158,7 @@ def message_handler(update, context):
     if text == 'Записаться':
         new_appointment(update, context)
 
+    # TODO организовать очерёдность салон/услуга/мастер в зависимости от выбранного варианта
     if text == 'По салону':
         by_salon(update, context)
 
@@ -126,6 +167,9 @@ def message_handler(update, context):
 
     if text == 'По мастеру':
         by_master(update, context)
+
+    if text == 'Политика обработки данных':
+        send_file_policy(update, context)
 
 
 if __name__ == '__main__':
