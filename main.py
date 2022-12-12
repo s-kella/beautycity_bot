5134a50d165b85bf6c6549dd299250e05d11039c
s-kella <s-kella@bk.ru>
2022-12-12 22:27:07 +0300

    replace buttons with InlineKeyboardButton

--- main.py
+++ main.py
@@ -47,13 +47,6 @@ def start(update, context):
     # registration(update, context)
 
 
-# def menu(update, context):
-#     buttons = ['Личный кабинет', 'Записаться']
-#     reply_markup = get_keyboard(buttons)
-#     update.message.reply_text(
-#         text="Главное меню:",
-#         reply_markup=reply_markup,
-#     )
 def main_menu(update: Update, context: CallbackContext):
     query = update.callback_query
     if query:
@@ -77,25 +70,18 @@ def main_menu(update: Update, context: CallbackContext):
         query.message.delete()
 
 
-# def lk(update, context):
-#     buttons = ['Мои записи', 'Прошлые записи', 'Назад']
-#     reply_markup = get_keyboard(buttons)
-#     update.message.reply_text(
-#         text="Личный кабинет:",
-#         reply_markup=reply_markup,
-#     )
 def account_menu(update: Update, context: CallbackContext):
     query = update.callback_query
     query.answer()
 
     message_text = bot_strings.account_menu
     keyboard = [
-        # [
-        #     InlineKeyboardButton(bot_strings.favorite_recipes_button, callback_data='favorite_recipes'),
-        # ],
-        # [
-        #     InlineKeyboardButton(bot_strings.excluded_recipes_button, callback_data='excluded_recipes'),
-        # ],
+        [
+            InlineKeyboardButton(bot_strings.my_appointments, callback_data='my_ap'),
+        ],
+        [
+            InlineKeyboardButton(bot_strings.past_appointments, callback_data='past_ap'),
+        ],
         [
             InlineKeyboardButton(bot_strings.back_button, callback_data='back_to_main'),
         ],
@@ -106,27 +92,46 @@ def account_menu(update: Update, context: CallbackContext):
 
 
 def my_appointments(update, context):
-    update.message.reply_text(
-        text="Мои записи:",
-    )
+    query = update.callback_query
+    query.answer()
+    message_text = bot_strings.my_appointments
+    keyboard = [
+        [
+            InlineKeyboardButton(bot_strings.back_button, callback_data='back_to_main'),
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.effective_chat.send_message(message_text, reply_markup=reply_markup)
+    query.message.delete()
 
 
-def past_appointments(update, context):
-    buttons = ['Запись 1', 'Запись 2', 'Назад']
-    reply_markup = get_keyboard(buttons)
-    update.message.reply_text(
-        text="Прошлые записи:",
-        reply_markup=reply_markup,
-    )
+def past_appointments(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+
+    try:
+        appointments = [{'id': 1,
+                         'name': 'Запись 1'},
+                        {'id': 2,
+                         'name': 'Запись 2'}]
+    except django.db.Error:
+        update.effective_chat.send_message(bot_strings.db_error_message)
+        return main_menu(update, context)
+
+    message_text = bot_strings.past_appointments
+    keyboard = [
+        [
+            InlineKeyboardButton(app['name'], callback_data=f'app{app["id"]}'),
+        ] for app in appointments
+    ]
+    keyboard.append([
+        InlineKeyboardButton(bot_strings.back_button, callback_data='back_to_main'),
+    ])
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.effective_chat.send_message(message_text, reply_markup=reply_markup)
+    query.message.delete()
 
 
-# def new_appointment(update, context):
-#     buttons = ['По салону', 'По  услуге', 'По мастеру', 'Назад']
-#     reply_markup = get_keyboard(buttons)
-#     update.message.reply_text(
-#         text="Новая запись",
-#         reply_markup=reply_markup,
-#     )
 def new_appointment(update: Update, context: CallbackContext):
     query = update.callback_query
     query.answer()
@@ -309,6 +314,8 @@ class Command(BaseCommand):
 
         dispatcher.add_handler(CallbackQueryHandler(account_menu, pattern=r'^account$'))
         dispatcher.add_handler(CallbackQueryHandler(new_appointment, pattern=r'^new_appointment$'))
+        dispatcher.add_handler(CallbackQueryHandler(past_appointments, pattern=r'^past_ap$'))
+        dispatcher.add_handler(CallbackQueryHandler(my_appointments, pattern=r'^my_ap$'))
         dispatcher.add_handler(CallbackQueryHandler(by_salon_menu, pattern=r'^by_salon$'))
 
         dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern=r'^main_menu$|^back_to_main$'))
